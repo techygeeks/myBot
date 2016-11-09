d466df14af7a51e6c74772b62c12a89b76aa55b7
techygeeks <vikasmarwaha90@gmail.com>
2016-11-09 15:49:12 +0530

    added code

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 var restify = require('restify'); 
 var builder = require('botbuilder'); 
 //var Client = require('node-rest-client').Client;
-
+var inputOfUser;
+var expEligible = 'false';
 //var client = new Client();
-
 /*var args = {
     data: {}, // data passed to REST method (only useful in POST, PUT or PATCH methods) 
     path: {}, // path substitution var 
@@ -59,12 +59,14 @@ bot.dialog('/', function (session) {
         );
     }
     else if(userInput.toLowerCase().indexOf('manager')!=-1){
+        inputOfUser = 'manager';
         session.send("Following jobs are available  "+
         " 1. HR Manager - HM1098  " +
         " 2. Engineering Manager - EM1099  " +
         " Enter Job Id(For E.g hm1098) to check details ");
     }
     else if(userInput.toLowerCase().indexOf('engineer')!=-1){
+        inputOfUser = 'engineer';
         session.send("Following jobs are available  " +
         " 1. Software Engineer - SE2011  "+
         " 2. Senior Software Engineer - SSE2012  "+
@@ -76,7 +78,8 @@ bot.dialog('/', function (session) {
         " 1. Compensation and Benefits " +
         " 2. Recruitment and Hiring " +
         " 3. Performance/Employee Evaluation " +
-        " 4. Training and Staff Development "
+        " 4. Training and Staff Development "+
+        " Type apply in case you want to apply for this job"
         );
     }
     else if(userInput.toLowerCase().indexOf('em1099')!=-1){
@@ -84,15 +87,17 @@ bot.dialog('/', function (session) {
         " 1. Project management " +
         " 2. Technical understanding " +
         " 3. Performance/Employee Evaluation " +
-        " 4. Management duties "
+        " 4. Management duties "+
+        " Type apply in case you want to apply for this job"
         );
     }
      else if(userInput.toLowerCase().indexOf('se2011')!=-1){
-         session.send("Software Engineer Required with an experience of over 2 years, Should have following skills/experience: " + 
+         session.send("Software Engineer Required with an experience of over 4 years, Should have following skills/experience: " + 
         " 1. Software Development " +
         " 2. Understanding of Frameworks such as Spring and Hibernate " +
         " 3. Coding Abilities " +
-        " 4. Algorithms and Data Structures "
+        " 4. Algorithms and Data Structures "+
+        " Type apply in case you want to apply for this job"
         );
     }
      else if(userInput.toLowerCase().indexOf('sse2012')!=-1){
@@ -100,12 +105,41 @@ bot.dialog('/', function (session) {
         " 1. Software Development " +
         " 2. Understanding of Frameworks such as Spring and Hibernate " +
         " 3. Coding Abilities " +
-        " 4. Algorithms and Data Structures "
+        " 4. Algorithms and Data Structures "+
+        " Type apply in case you want to apply for this job"
         );
     }
     else if(userInput.toLowerCase().indexOf('apply')!=-1)
-    {
-        session.send("Link for sending application - https://linkedin.com/apply/jobs/bot");
+    {   
+        expEligible = 'false';
+        session.send("Before i send your resume, I need you to answer some questions" +
+        " What is you current experience in organization(in months for E.g 12 for 12 months)"
+        );
+    }
+    else if(expEligible=='true' && inputOfUser=="manager"){
+        if(userInput>10){
+            //session.send("What is your qualification 1. Graduate 2. PostGraduate ,type in name of qualification");
+            session.send("Link for application : https://wfr.kronos.com/apply/job/bot")
+        }
+        else{
+            session.send("Sorry you are not eligible for this position")
+        }
+    }
+     else if(expEligible=='true' && inputOfUser=="engineer"){
+        if(userInput>4){
+            session.send("Link for application : https://wfr.kronos.com/apply/job/bot")
+        }
+        else{
+            session.send("Sorry you are not eligible for this position")
+        }
+    }
+    else if(userInput>18){
+        expEligible = 'true';
+        session.send("What is your total Experience in years");
+    }
+    else if(userInput<=18){
+        expEligible = 'false';
+        session.send("You are not eligible for this job as you have not spent more than 18 months in the organization");
     }
     else if((userInput.toLowerCase().indexOf('hi')!=-1)||(userInput.toLowerCase().indexOf('hello')!=-1)){
         session.send("Hello User,How can i help you today");
