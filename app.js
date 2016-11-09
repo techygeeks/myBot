1f93513008b69ef1c18fad75c21dd6baf3ee75af
techygeeks <vikasmarwaha90@gmail.com>
2016-11-09 14:47:48 +0530

    updated code

--- app.js
+++ app.js
@@ -59,47 +59,47 @@ bot.dialog('/', function (session) {
     }
     else if(userInput.toLowerCase().indexOf('manager')!=-1){
         session.send("1. Following jobs are available  "+
-        "1. HR Manager - HM1098  " +
-        "2. Engineering Manager - EM1099  " +
+        " 1. HR Manager - HM1098  " +
+        " 2. Engineering Manager - EM1099  " +
         " Enter Job Id(For E.g hm1098) to check details ");
     }
     else if(userInput.toLowerCase().indexOf('engineer')!=-1){
         session.send("1. Following jobs are available  " +
-        "1. Software Engineer - SE2011  "+
-        "2. Senior Software Engineer - SSE2012  "+
+        " 1. Software Engineer - SE2011  "+
+        " 2. Senior Software Engineer - SSE2012  "+
         " Enter Job Id(For E.g se2011) to check details "
         );
     }
     else if(userInput.toLowerCase().indexOf('hm1098')!=-1){
-        session.send("HR Manager Required with an experience of over 10 years, Should have following skills/experience:" + 
-        "1. Compensation and Benefits" +
-        "2. Recruitment and Hiring" +
-        "3. Performance/Employee Evaluation" +
-        "4. Training and Staff Development"
+        session.send("HR Manager Required with an experience of over 10 years, Should have following skills/experience:  " + 
+        " 1. Compensation and Benefits " +
+        " 2. Recruitment and Hiring " +
+        " 3. Performance/Employee Evaluation " +
+        " 4. Training and Staff Development "
         );
     }
     else if(userInput.toLowerCase().indexOf('em1099')!=-1){
-         session.send("Engineering Manager Required with an experience of over 10 years, Should have following skills/experience:" + 
-        "1. Project management" +
-        "2. Technical understanding" +
-        "3. Performance/Employee Evaluation" +
-        "4. Management duties"
+         session.send("Engineering Manager Required with an experience of over 10 years, Should have following skills/experience: " + 
+        " 1. Project management " +
+        " 2. Technical understanding " +
+        " 3. Performance/Employee Evaluation " +
+        " 4. Management duties "
         );
     }
      else if(userInput.toLowerCase().indexOf('se2011')!=-1){
-         session.send("Software Engineer Required with an experience of over 2 years, Should have following skills/experience:" + 
-        "1. Software Development" +
-        "2. Understanding of Frameworks such as Spring and Hibernate" +
-        "3. Coding Abilities" +
-        "4. Algorithms and Data Structures"
+         session.send("Software Engineer Required with an experience of over 2 years, Should have following skills/experience: " + 
+        " 1. Software Development " +
+        " 2. Understanding of Frameworks such as Spring and Hibernate " +
+        " 3. Coding Abilities " +
+        " 4. Algorithms and Data Structures "
         );
     }
      else if(userInput.toLowerCase().indexOf('sse2012')!=-1){
-         session.send("Senior Software Engineer Required with an experience of over 4 years, Should have following skills/experience:" + 
-        "1. Software Development" +
-        "2. Understanding of Frameworks such as Spring and Hibernate" +
-        "3. Coding Abilities" +
-        "4. Algorithms and Data Structures"
+         session.send("Senior Software Engineer Required with an experience of over 4 years, Should have following skills/experience: " + 
+        " 1. Software Development " +
+        " 2. Understanding of Frameworks such as Spring and Hibernate " +
+        " 3. Coding Abilities " +
+        " 4. Algorithms and Data Structures "
         );
     }
     else if(userInput.toLowerCase().indexOf('apply')!=-1)
